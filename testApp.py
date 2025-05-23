7eaee1c2f6739b510fa51913c2eef68e9e751ef3
natlusbrr . <imexpert2004@outlook.com>
2025-05-23 09:08:53 +0100

    save all work

--- testApp.py
+++ testApp.py
@@ -19,7 +19,19 @@ class TaskTrackerTest(unittest.TestCase):
         response = self.client.delete('/tasks/1')
         self.assertEqual(response.status_code, 200)
 
+    #purpose test fails
 
+    def test_wrong_status_code(self): 
+        response = self.client.post('/tasks', json={'title': 'this should fail'}) 
+        self.assertEqual(response.status_code, 200) # Should be 201
+
+    def test_missing_title(self): 
+        response = self.client.post('/tasks', json={}) # title left blank 
+        self.assertEqual(response.status_code, 400)
+
+    def test_delete_nonexistent_task(self): 
+        response = self.client.delete('/tasks/999') # specifying id taht doesnt exist 
+        self.assertEqual(response.status_code, 404)
 
 if __name__ == '__main__':
     unittest.main()
